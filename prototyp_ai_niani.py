314b47647a3eea460667c967d815a276fcaba148
nekio <konradsokol552@gmail.com>
2026-07-18 16:43:08 +0200

    Update prototyp_ai_niani.py

--- prototyp_ai_niani.py
+++ prototyp_ai_niani.py
@@ -208,8 +208,7 @@ def screen_parent():
     st.divider()
     
     # ZMIANA: Powrót do menu zamiast resetu sesji
-    if st.button("wyjdź", #type="primary", use_container_width=True ):
-    ):
+    if st.button("wyjdź", ):
         st.session_state.role = "login"
         st.rerun()
         
